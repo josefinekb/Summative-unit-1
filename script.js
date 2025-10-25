20cbd2dde04e4d92538aa19ebe47ae260a4445ab
josefinekb <josefine.b@cismoss.no>
2025-10-25 12:07:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const facts = [
-  "I love drawing and creating digital art!",<img>
+  "Sheltie",<img> https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.purina.co.uk%2Ffind-a-pet%2Fdog-breeds%2Fshetland-sheepdog&psig=AOvVaw0JPhqG9OG-BiNBUhSOnJTQ&ust=1761473160768000&source=images&cd=vfe&opi=89978449&ved=0CBQQjhxqFwoTCJCc_oKNv5ADFQAAAAAdAAAAABAL
+  "My favorite sport is basketball.",<img>
+  "I enjoy coding small games.",<img>
+  "I like to bake cupcakes on weekends."<img>
   "My favorite sport is basketball.",<img>
   "I enjoy coding small games.",<img>
   "I like to bake cupcakes on weekends."<img>
@@ -9,9 +12,15 @@ const facts = [
   "My favorite sport is basketball.",<img>
   "I enjoy coding small games.",<img>
   "I like to bake cupcakes on weekends."<img>
+  "My favorite sport is basketball.",<img>
+  "I enjoy coding small games.",<img>
+  "I like to bake cupcakes on weekends."<img>
 ];
 const facts = [
   "I love drawing and creating digital art!",<img>
+  "My favorite sport is basketball.",<img>
+  "I enjoy coding small games.",<img>
+  "I like to bake cupcakes on weekends."<img>
   "My favorite sport is basketball.",<img>
   "I enjoy coding small games.",<img>
   "I like to bake cupcakes on weekends."<img>
