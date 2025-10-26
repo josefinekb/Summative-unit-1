d2281b1b4773bde776b8efd0c33f2cdd05be2a92
josefinekb <josefine.b@cismoss.no>
2025-10-26 09:42:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 const fact = [
   <button type="button">Show rabbit breed!</button>
 <figure>
-  <img src="https://placekitten.com/200/200" />
+  <img src="https://www.pinterest.com/pin/94505292175616100//200/200" />
   <figcaption>This is a Thrianta rabbit!</figcaption>
 </figure>
 <figure>
-  <img src="https://placekitten.com/200/200" />
+  <img src="https://www.pinterest.com/pin/2322237301001824//200/200" />
   <figcaption>This is a American Fuzzy Lop!</figcaption>
 </figure>
 <figure>
-  <img src="https://placekitten.com/200/200" />
+  <img src="https://www.pinterest.com/pin/49469295904440026//200/200" />
   <figcaption>This is a Lionhead rabbit!</figcaption>
 </figure>
 <figure>
-  <img src="https://placekitten.com/200/200" />
+  <img src="https://petcorner.pangovet.com/wp-content/uploads/2024/06/Alaska-Rabbit-1.webp/200/200" />
   <figcaption>This is a Alaskan rabbit!</figcaption>
 </figure>
 <figure>
-  <img src="https://placekitten.com/200/200" />
-  <figcaption>This is a Lionhead rabbit!</figcaption>
+  <img src="https://images.squarespace-cdn.com/content/v1/5f11acca056d9b31bdc66141/9a5782d5-17f9-4dec-a34a-84ffeb0abac3/IMG_0354.jpg?format=1000w/200/200" />
+  <figcaption>This is a Netherland Dwarf rabbit!</figcaption>
 </figure>
 ];
 
