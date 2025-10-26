a7c53cee2c24fabcd7e8e342cff6a13ab986f4be
josefinekb <josefine.b@cismoss.no>
2025-10-26 09:10:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,69 +1,14 @@
-const dog = [
-  "Sheltie",<img> 
-  "My favorite sport is basketball.",<img>
-  "I enjoy coding small games.",<img>
-  "I like to bake cupcakes on weekends."<img>
-  "My favorite sport is basketball.",<img>
-  "I enjoy coding small games.",<img>
-  "I like to bake cupcakes on weekends."<img>
+const fact = [
+  "This is a Thrianta rabbit!",
+  "This is a American Fuzzy Lop!",
+  "This is a Lionhead rabbit!",
+  "This is a Alaskan rabbit!"
 ];
-const showImage = () => {
-  document.getElementById("first").style.display = 'block';
-}
 
-<button onClick="Show dog!">Button</button>
-<div id="first" style="height:400px; width:400px; display:none;">
-  <img src="https://www.nkk.no/getfile.php/136744-1486108328/Bilder/Rasebilder/Shetland%20sheepdog/Shetland%20sheepdog_vibeke%20brath.jpg%20%28dog_race_profile%29.jpg/400/400" />
-  </div> 
-  <figcaption>This is a sheltie dog.</figcaption>
-</figure>
-  
-const cat = [
-  "I love drawing and creating digital art!",<img>
-  "My favorite sport is basketball.",<img>
-  "I enjoy coding small games.",<img>
-  "I like to bake cupcakes on weekends."<img>
-  "My favorite sport is basketball.",<img>
-  "I enjoy coding small games.",<img>
-  "I like to bake cupcakes on weekends."<img>
-];
-  const showImage = () => {
-  document.getElementById("first").style.display = 'block';
-}
+const factParagraph = document.getElementById("rabbit");
+const button = document.getElementById("changeButton");
 
-<button onClick="showImage()">Button</button>
-<div id="first" style="height:400px; width:400px; display:none;">
-  <img src="https://placekitten.com/400/400" />
-</div>
-    
-<button type="button">Show example</button>
-<figure>
-  <img src="https://placekitten.com/200/200" />
-  <figcaption>This is a lovely kitten.</figcaption>
-</figure>
-
-const rabbit = [
-  "I love drawing and creating digital art!",<img>
-  "My favorite sport is basketball.",<img>
-  "I enjoy coding small games.",<img>
-  "I like to bake cupcakes on weekends."<img>
-  "My favorite sport is basketball.",<img>
-  "I enjoy coding small games.",<img>
-  "I like to bake cupcakes on weekends."<img>
-];
-const showImage = () => {
-  document.getElementById("first").style.display = 'block';
-}
-
-<button onClick="showImage()">Button</button>
-<div id="first" style="height:400px; width:400px; display:none;">
-  <img src="https://placekitten.com/400/400" />
-</div>
-
-<button type="button">Show rabbit!</button>
-<figure>
-  <img src="https://placekitten.com/200/200" />
-  <figcaption>This is a angora rabbit.</figcaption>
-</figure>
+button.addEventListener("click", () => {
+  const randomIndex = Math.floor(Math.random() * fact.length);
+  factParagraph.textContent = fact[randomIndex];
 });
-
