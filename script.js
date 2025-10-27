630136c0042818c5c297b3f00b9d57ecd0e2a3ad
josefinekb <josefine.b@cismoss.no>
2025-10-27 08:34:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,4 +12,13 @@ const button = document.getElementById("changeButton");
 button.addEventListener("click", () => {
   const randomIndex = Math.floor(Math.random() * facts.length);
   factParagraph.textContent = facts[randomIndex];
+  
+  button.addEventListener("click", () => {
+  const randomIndex = Math.floor(Math.random() * facts.length);
+  factParagraph.textContent = facts[randomIndex];
+  button.textContent = "Click again for more!";
+
+    const colorButton = document.getElementById("colorButton");
+colorButton.addEventListener("click", () => {
+  document.querySelector(".card").style.backgroundColor = "#ffd700";
 });
